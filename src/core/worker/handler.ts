991fb13344744f072015e687a88972cdca975f83
qianbin <cola.tin.com@gmail.com>
2020-04-27 00:24:10 +0800

    add more methods in worker

--- src/core/worker/handler.ts
+++ src/core/worker/handler.ts
@@ -1,16 +1,43 @@
-import { randomBytes, createHmac } from 'crypto'
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const m = require('more-entropy')
+import { HDNode } from 'thor-devkit/dist/cry/hdnode'
+import { pbkdf2Sync } from 'crypto'
+
+async function handleCommand(cmd: string, arg: any): Promise<any> {
+    if (cmd === 'collectEntropy') {
+        // generates 256-bit random key which is secured by extra CPU based entropy.
+        return new Promise(resolve => {
+            new m.Generator().generate(512, (entropy: number[]) => {
+                resolve(Int16Array.from(entropy))
+            })
+        })
+    } else if (cmd === 'deriveAddress') {
+        const [pub, chainCode, index] = arg
+        return HDNode
+            .fromPublicKey(Buffer.from(pub), Buffer.from(chainCode))
+            .derive(index)
+            .address
+    } else if (cmd === 'derivePrivateKey') {
+        const [words, index] = arg
+        return HDNode
+            .fromMnemonic(words)
+            .derive(index)
+            .privateKey
+    } else if (cmd === 'kdf') {
+        const [password, salt, n] = arg
+        return pbkdf2Sync(password, Buffer.from(salt), n, 32, 'sha256')
+    }
+    throw new Error(`worker: unexpected command '${cmd}'`)
+}
 
 const ctx: Worker = self as never
-
-ctx.onmessage = (ev) => {
-    if (ev.data[0] === 'generateKey') {
-        // generates 256-bit random key which is secured by extra CPU based entropy.
-        new m.Generator().generate(512, (entropy: number[]) => {
-            const mac = createHmac('sha256', randomBytes(32))
-            const key = mac.update(Int16Array.from(entropy)).digest()
-            ctx.postMessage(Uint8Array.from(key))
-        })
+ctx.onmessage = async (ev) => {
+    const [cmd, arg] = ev.data
+    try {
+        const result = await handleCommand(cmd, arg)
+        ctx.postMessage([result])
+    } catch (err) {
+        ctx.postMessage([undefined, err])
     }
 }
--- src/core/worker/index.ts
+++ src/core/worker/index.ts
@@ -2,11 +2,34 @@ import Worker from 'worker-loader!./handler'
 
 const worker = new Worker()
 
-export function generateKey() {
-    return new Promise<Buffer>(resolve => {
-        worker.postMessage(['generateKey'])
+function call<R>(cmd: string, ...args: unknown[]): Promise<R> {
+    return new Promise<R>((resolve, reject) => {
+        worker.postMessage([cmd, args])
         worker.onmessage = ev => {
-            resolve(Buffer.from(ev.data))
+            const [result, err] = ev.data
+            if (err) {
+                reject(err)
+            } else {
+                resolve(result)
+            }
         }
     })
+}
+
+export function collectEntropy() {
+    return call<Int16Array>('collectEntropy')
+}
+
+export function deriveAddress(pub: Buffer, chainCode: Buffer, index: number) {
+    return call<string>('deriveAddress', pub, chainCode, index)
+}
+
+export function derivePrivateKey(words: string[], index: number) {
+    return call<Uint8Array>('derivePrivateKey', words, index)
+        .then(k => Buffer.from(k))
+}
+
+export function kdf(password: string, salt: Buffer, n: number) {
+    return call<Uint8Array>('kdf', password, salt, n)
+        .then(k => Buffer.from(k))
 }
