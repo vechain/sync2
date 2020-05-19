91dea9de193348aaf48638f2e4928d749c1951ad
qianbin <cola.tin.com@gmail.com>
2020-05-20 00:24:04 +0800

    comp: implement ConnexAccount

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,13 +3,15 @@ import AddressCard from './AddressCard.vue'
 import AddressCarousel from './AddressCarousel.vue'
 import Tokens from './Tokens.vue'
 import MainDrawerContent from './MainDrawerContent.vue'
+import ConnexAccount from './ConnexAccount.vue'
 
 const components: { [name: string]: Vue.VueConstructor } = {
     StackedRouterView,
     AddressCard,
     AddressCarousel,
     Tokens,
-    MainDrawerContent
+    MainDrawerContent,
+    ConnexAccount
 }
 
 export default components
