3ea1f7bd2e05bcbcbbaae9558ba99ca71edd646c
Asbert Ma <stayinalive@msn.cn>
2020-05-19 16:40:17 +0800

    register components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,15 @@
 import StackedRouterView from './StackedRouterView.vue'
-import AccountCard from './AccountCard.vue'
+import AddressCard from './AddressCard.vue'
+import AddressCarousel from './AddressCarousel.vue'
+import Tokens from './Tokens.vue'
+import MainDrawerContent from './MainDrawerContent.vue'
 
 const components: { [name: string]: Vue.VueConstructor } = {
     StackedRouterView,
-    AccountCard
+    AddressCard,
+    AddressCarousel,
+    Tokens,
+    MainDrawerContent
 }
 
 export default components
