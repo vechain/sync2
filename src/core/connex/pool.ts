ab277d7256222413d9b567156888b3a673285f1b
qianbin <cola.tin.com@gmail.com>
2020-05-22 13:42:59 +0800

    core/connex: implement ref count and continuous query

--- src/core/connex/pool.ts
+++ src/core/connex/pool.ts
@@ -2,10 +2,16 @@ import { Framework } from '@vechain/connex-framework'
 import { DriverNoVendor } from '@vechain/connex-driver/dist/driver-no-vendor'
 import { SimpleNet } from '@vechain/connex-driver/dist/simple-net'
 
+interface Node {
+    gid: string
+    url: string
+}
+
 interface Instance {
     connexPromise: Promise<Connex>
-    close: () => void
-    node: Node
+    destroy(): void
+    refCount: number
+    timer?: NodeJS.Timeout
 }
 
 function newInstance(node: Node): Instance {
@@ -13,60 +19,92 @@ function newInstance(node: Node): Instance {
     const connexPromise = (async () => {
         const net = new SimpleNet(node.url)
         const genesis: Connex.Thor.Block = await net.http('GET', 'blocks/0')
-        if (genesis.id !== node.genesisId) {
+        if (genesis.id !== node.gid) {
             throw new Error('genesis id mismatch')
         }
 
         driver = new DriverNoVendor(net, genesis)
-        const framework = new Framework(driver)
+        try {
+            const framework = new Framework(driver)
 
-        // make sure the returned framework is warmed up
-        await framework.thor.block().get()
+            const warmup = async () => {
+                const ticker = framework.thor.ticker()
+                while (framework.thor.status.head.number === 0) {
+                    await ticker.next()
+                }
+            }
+            // make sure the returned framework is warmed up
+            await Promise.race([
+                warmup(),
+                new Promise(resolve => setTimeout(resolve, 20 * 1000))
+                    .then(() => { throw new Error('timeout to init connex') })
+            ])
 
-        return framework
+            return framework
+        } catch (err) {
+            driver.close()
+            throw err
+        }
     })()
+
     return {
         connexPromise,
-        close: () => driver && driver.close(),
-        node
+        destroy: () => { driver && driver.close() },
+        refCount: 0
     }
 }
 
-const instances: Record<string, Instance | undefined> = {}
+const instances: Record<string, Instance> = {}
 
-interface Node {
-    genesisId: string
-    url: string
+export interface ConnexRef {
+    connex: Connex
+    release(): void
 }
 
 /**
- * retain a reused connex object
+ * retain a reference counted reusable connex object.
  * @param node the node config
  */
-export async function retain(node: Node) {
+export async function retain(node: Node): Promise<ConnexRef> {
     node = { ...node }
-    const key = node.genesisId
+    const key = `${node.gid}|${node.url}`
 
     let instance = instances[key]
     if (!instance) {
         instance = newInstance(node)
         instances[key] = instance
-    } else {
-        if (instance.node.url !== node.url) {
-            // simple close the old instance with difference url,
-            // and create a new one
-            instance.close()
-            instance = newInstance(node)
-            instances[key] = instance
-        }
     }
-    // TODO: shutdown idle instance
 
     try {
-        return await instance.connexPromise
+        const connex = await instance.connexPromise
+
+        instance.refCount++
+        // kill the timer when retain again, since destroying is delayed
+        if (instance.timer) {
+            clearTimeout(instance.timer)
+            instance.timer = undefined
+        }
+
+        let releaseCalled = false
+        return {
+            connex,
+            release: () => {
+                if (releaseCalled) {
+                    throw new Error('connexRef is already released')
+                }
+                releaseCalled = true
+                instance.refCount--
+                if (instance.refCount === 0) {
+                    instance.timer = setTimeout(() => {
+                        delete instances[key]
+                        instance.destroy()
+                    }, 60 * 1000)
+                }
+            }
+        }
     } catch (err) {
-        instance.close()
         delete instances[key]
+        instance.destroy()
         throw err
     }
 }
