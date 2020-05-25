b38bfe9f974c42cd81daa4ddc3b703b33be6c4fd
qianbin <cola.tin.com@gmail.com>
2020-05-25 22:54:41 +0800

    boot: add $authenticate method

--- src/boot/misc.ts
+++ src/boot/misc.ts
@@ -1,11 +1,13 @@
 import { boot } from 'quasar/wrappers'
 import * as State from 'src/state'
 import { BioPass } from 'src/utils/bio-pass'
+import AuthenticationDialog from 'pages/AuthenticationDialog.vue'
 
 declare module 'vue/types/vue' {
     interface Vue {
         $state: ReturnType<typeof State.build>
         $bioPass: BioPass | null
+        $authenticate<T>(task: (password: string) => Promise<T>): Promise<T>
     }
 }
 
@@ -19,5 +21,22 @@ export default boot(async ({ Vue }) => {
     const bioPass = await BioPass.init('main')
     Object.defineProperty(Vue.prototype, '$bioPass', {
         get() { return bioPass }
+    })
+
+    Object.defineProperty(Vue.prototype, '$authenticate', {
+        get(): Vue['$authenticate'] {
+            const vm = this as Vue
+            return task => {
+                return new Promise((resolve, reject) => {
+                    vm.$q.dialog({
+                        component: AuthenticationDialog,
+                        parent: vm,
+                        task
+                    })
+                        .onOk(resolve)
+                        .onCancel(() => reject(new Error('cancelled')))
+                })
+            }
+        }
     })
 })
