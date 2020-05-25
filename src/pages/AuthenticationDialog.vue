5b69b22f1fa24b1240589a17a2efc2ac2a9962e5
qianbin <cola.tin.com@gmail.com>
2020-05-26 00:49:50 +0800

    pages/AuthenticationDialog: handle task

--- src/pages/AuthenticationDialog.vue
+++ src/pages/AuthenticationDialog.vue
@@ -18,11 +18,14 @@
                 >Cancel</q-btn>
             </q-toolbar>
             <q-space />
-            <pin-code-input @input="ok($event)" />
+            <pin-code-input
+                v-model="code"
+                @fulfilled="runTask($event)"
+            />
             <q-btn
-                :icon="$bioPass.authType==='face'?'sentiment_satisfied': 'fingerprint'"
-                v-if="hasBioPass===true"
-                @click="bioPassRecall"
+                v-if="bioPassReady"
+                :icon="bioAuthTypeIcon"
+                @click="recallBioPass"
                 flat
             />
             <q-space />
@@ -32,40 +35,62 @@
 <script lang="ts">
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import Vue from 'vue'
+import { QSpinnerIos } from 'quasar'
 
 export default Vue.extend({
+    props: {
+        task: { type: Function as unknown as () => (<T>(password: string) => Promise<T>) }
+    },
     data: () => {
         return {
-            hasBioPass: null as boolean | null
+            bioPassReady: false,
+            code: ''
+        }
+    },
+    computed: {
+        bioAuthTypeIcon() {
+            if (this.$bioPass) {
+                return this.$bioPass.authType === 'face' ? 'sentiment_satisfied' : 'fingerprint'
+            }
+            return ''
         }
     },
     methods: {
         show() { (this.$refs.dialog as any).show() },
         hide() { (this.$refs.dialog as any).hide() },
-        ok(password: string) {
-            this.$emit('ok', { password })
+        ok(result: unknown) {
+            this.$emit('ok', result)
             this.hide()
         },
-        async bioPassRecall() {
+        async recallBioPass() {
             if (this.$bioPass) {
                 try {
                     const password = await this.$bioPass.recall('recall password')
-                    this.ok(password)
+                    await this.runTask(password)
                 } catch (err) {
                     console.warn(err)
                 }
+            }
+        },
+        async runTask(password: string) {
+            try {
+                this.$q.loading.show({ spinner: QSpinnerIos as unknown as Vue, delay: 100 })
+                this.ok(await this.task(password))
+            } catch (err) {
+                this.code = ''
+                console.log('run task error:', err)
+            } finally {
+                this.$q.loading.hide()
             }
         }
     },
     async created() {
         if (this.$bioPass) {
             try {
-                this.hasBioPass = await this.$bioPass.has()
+                this.bioPassReady = await this.$bioPass.has()
             } catch (err) {
                 console.warn(err)
             }
-        } else {
-            this.hasBioPass = false
         }
     }
 })
