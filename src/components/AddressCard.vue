0775b91cd1a4cc14387779a9ccd0ade429500b4a
qianbin <cola.tin.com@gmail.com>
2020-06-13 17:25:11 +0800

    comp/AddressCard: tweak style and display balances

--- src/components/AddressCard.vue
+++ src/components/AddressCard.vue
@@ -9,10 +9,25 @@
             :query="()=> connex.thor.account(address).get()"
             v-slot="{data}"
         >
-            <q-card-section class="text-white">
-                <div> #{{index}}</div>
-                <div class="ellipsis"> {{address}}</div>
-                {{data}}
+            <q-card-section class="text-white column no-wrap full-height">
+                <!-- logo -->
+                <img
+                    src="~assets/vechain-logo.svg"
+                    class="brand-icon q-mb-sm"
+                    width="48"
+                    height="48"
+                >
+                <!-- address -->
+                <span class="monospace text-h6"> {{address | checksum | abbrev(8,6)}}</span>
+                <q-space />
+                <!-- balances -->
+                <div class="text-right">
+                    <span class="serif text-h6">{{data?data.balance:null | balance}}</span>
+                    <span class="monospace text-caption q-ml-sm">VET&nbsp;</span></div>
+                <div class="text-right">
+                    <span class="serif text-h6">{{data?data.energy:null | balance}}</span>
+                    <span class="monospace text-caption q-ml-sm">VTHO</span>
+                </div>
             </q-card-section>
         </ConnexContinuous>
     </q-card>
@@ -28,11 +43,13 @@ export default Vue.extend({
         connex: Object as () => Connex
     },
     computed: {
-        background() {
-            const svg = picasso(this.address)
+        svg() {
+            return picasso(this.address)
+        },
+        background(): object {
             const backgrounds = [
-                'radial-gradient(circle, grey 0%, black 100%)',
-                `url('data:image/svg+xml;utf8,${svg}')`
+                'linear-gradient(160deg, grey 0%, black 100%)',
+                `url('data:image/svg+xml;utf8,${this.svg}')`
             ]
             return {
                 background: backgrounds.join(','),
@@ -44,3 +61,8 @@ export default Vue.extend({
     }
 })
 </script>
+<style scoped>
+.brand-icon {
+    opacity: 0.7;
+}
+</style>
