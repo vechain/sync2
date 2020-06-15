a3a09392082726d58a524c50108df4ffa77927e6
qianbin <cola.tin.com@gmail.com>
2020-06-15 14:03:42 +0800

    comp/AddressCard: tweak logo style

--- src/components/AddressCard.vue
+++ src/components/AddressCard.vue
@@ -13,23 +13,27 @@
                 <!-- logo -->
                 <img
                     src="~assets/vechain-logo.svg"
-                    class="brand-icon q-mb-sm"
-                    width="48"
-                    height="48"
+                    class="logo absolute-bottom-right"
+                    :width="height*0.8"
+                    :height="height*0.8"
                 >
-                <!-- address -->
-                <span class="monospace text-h6"> {{address | checksum | abbrev(8,6)}}</span>
-                <q-space />
                 <!-- balances -->
                 <div class="text-right">
-                    <span class="serif text-h6">{{data?data.balance:null | balance}}</span>
-                    <span class="monospace text-caption q-ml-sm">VET&nbsp;</span></div>
+                    <span class="text-h6 text-weight-regular">{{data?data.balance:null | balance}}</span>
+                    <span class="monospace text-caption"> VET&nbsp;</span></div>
                 <div class="text-right">
-                    <span class="serif text-h6">{{data?data.energy:null | balance}}</span>
-                    <span class="monospace text-caption q-ml-sm">VTHO</span>
+                    <span class="text-h6 text-weight-regular">{{data?data.energy:null | balance}}</span>
+                    <span class="monospace text-caption"> VTHO</span>
+                </div>
+                <q-space />
+                <!-- address -->
+                <div>
+                    <span class="text-h2 text-weight-thin">{{index+1}} </span>
+                    <span class="monospace text-overline">{{address | checksum | abbrev(8,6)}}</span>
                 </div>
             </q-card-section>
         </ConnexContinuous>
+        <q-resize-observer @resize="onResize" />
     </q-card>
 </template>
 <script lang="ts">
@@ -41,6 +45,11 @@ export default Vue.extend({
         address: String,
         index: Number,
         connex: Object as () => Connex
+    },
+    data: () => {
+        return {
+            height: 0
+        }
     },
     computed: {
         svg() {
@@ -58,11 +67,18 @@ export default Vue.extend({
                 backgroundBlendMode: 'soft-light'
             }
         }
+    },
+    methods: {
+        onResize(size: { height: number }) {
+            this.height = size.height
+        }
     }
 })
 </script>
 <style scoped>
-.brand-icon {
-    opacity: 0.7;
+.logo {
+    opacity: 0.12;
+    margin-right: -3%;
+    margin-bottom: -9%;
 }
 </style>
