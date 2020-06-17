dae40ce907ecb9087f8013ae0d31fef20b8c24b3
qianbin <cola.tin.com@gmail.com>
2020-06-17 16:30:15 +0800

    pages/Index: add 'new account' into menu

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,6 +5,7 @@
     >
         <q-toolbar>
             <q-toolbar-title> {{wallet.meta.name}}</q-toolbar-title>
+            <!-- menu -->
             <q-btn
                 flat
                 dense
@@ -12,24 +13,24 @@
                 icon="more_horiz"
                 aria-label="More"
             >
-                <q-menu>
-                    <q-list>
-                        <q-item>
-                            <q-item-section>Accounts</q-item-section>
-                        </q-item>
-                        <q-item>
-                            <q-item-section>Activity</q-item-section>
-                        </q-item>
-                        <q-item
-                            v-close-popup
-                            :to="{name: 'backup'}"
-                        >
-                            <q-item-section>Backup</q-item-section>
-                        </q-item>
-                        <q-item>
-                            <q-item-section>Delete</q-item-section>
-                        </q-item>
-                    </q-list>
+                <q-menu auto-close>
+                    <!-- limit to 10 accounts -->
+                    <q-item
+                        v-if="addresses.length<10"
+                        clickable
+                        @click="onClickNewAccount"
+                    >
+                        <q-item-section no-wrap>New Account</q-item-section>
+                    </q-item>
+                    <q-item>
+                        <q-item-section no-wrap>Activity</q-item-section>
+                    </q-item>
+                    <q-item :to="{name: 'backup'}">
+                        <q-item-section no-wrap>Backup</q-item-section>
+                    </q-item>
+                    <q-item>
+                        <q-item-section no-wrap>Delete</q-item-section>
+                    </q-item>
                 </q-menu>
             </q-btn>
         </q-toolbar>
@@ -37,8 +38,10 @@
             :node="node"
             v-slot="{connex}"
         >
+            <!-- address cart list -->
             <div
                 id="list"
+                ref="list"
                 class="row justify-center overflow-auto card-container"
             >
                 <Intersecting
@@ -56,12 +59,6 @@
                         @click="onClickAddress(i)"
                     />
                 </Intersecting>
-                <div class="q-ma-md card-wrap row flex-center">
-                    <q-btn
-                        flat
-                        @click="onNewAddress"
-                    >New</q-btn>
-                </div>
             </div>
         </ConnexObject>
     </div>
@@ -83,18 +80,30 @@ export default Vue.extend({
         }
     },
     methods: {
-        async onNewAddress() {
-            const wallet = this.wallet
-            const vault = await Vault.decode(wallet.vault)
-            const newAddress = (await vault.derive(wallet.meta.addresses.length)).address
-            const newMeta: M.Wallet.Meta = {
-                ...wallet.meta,
-                addresses: [...wallet.meta.addresses, newAddress]
-            }
+        onClickNewAccount() {
+            this.$loading(async () => {
+                const wallet = this.wallet
+                const addresses = wallet.meta.addresses
 
-            await this.$storage.wallets.update(
-                { id: wallet.id },
-                { meta: JSON.stringify(newMeta) })
+                // limit to 10 accounts
+                if (addresses.length >= 10) {
+                    return
+                }
+                const vault = await Vault.decode(wallet.vault)
+                const newAddress = (await vault.derive(addresses.length)).address
+                const newMeta: M.Wallet.Meta = {
+                    ...wallet.meta,
+                    addresses: [...addresses, newAddress]
+                }
+
+                await this.$storage.wallets.update(
+                    { id: wallet.id },
+                    { meta: JSON.stringify(newMeta) })
+
+                await new Promise(resolve => setTimeout(resolve, 300))
+                const list = this.$refs.list as HTMLElement
+                list.scrollTo({ top: list.scrollHeight, behavior: 'smooth' })
+            })
         },
         onClickAddress(index: number) {
             this.$router.push({
@@ -116,6 +125,7 @@ export default Vue.extend({
 <style scoped>
 .card-container {
     scroll-snap-type: y mandatory;
+    padding-bottom: calc(var(--card-width) * 0.67);
 }
 .card-wrap {
     width: var(--card-width);
