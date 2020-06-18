91d64d3211007c540c868c735f159c3c1def2bef
qianbin <cola.tin.com@gmail.com>
2020-06-18 14:59:54 +0800

    pages/ActionSheets: allow to set item class

--- src/pages/ActionSheets.vue
+++ src/pages/ActionSheets.vue
@@ -6,16 +6,25 @@
     >
         <q-card>
             <q-list>
-                <q-item
-                    clickable
-                    v-for="(a, i) in actions"
-                    :key="i"
-                    @click="ok();a.onClick()"
-                >
-                    <q-item-section class="text-subtitle1 text-center q-py-sm">
-                        {{a.label}}
-                    </q-item-section>
-                </q-item>
+                <template v-for="(a, i) in actions">
+                    <q-separator
+                        v-if="a.label === '-'"
+                        :key="i"
+                    />
+                    <q-item
+                        v-else
+                        :clickable="!!a.onClick"
+                        :key="i"
+                        @click="ok();a.onClick()"
+                    >
+                        <q-item-section
+                            class="text-subtitle1 text-center q-py-sm"
+                            :class="a.classes"
+                        >
+                            {{a.label}}
+                        </q-item-section>
+                    </q-item>
+                </template>
             </q-list>
         </q-card>
     </q-dialog>
@@ -26,7 +35,7 @@ import Vue from 'vue'
 
 export default Vue.extend({
     props: {
-        actions: Array as () => Array<{ label: string, onClick: Function }>
+        actions: Array as () => Array<{ label: string, classes?: string | string[], onClick?: Function }>
     },
     methods: {
         // method is REQUIRED by $q.dialog
