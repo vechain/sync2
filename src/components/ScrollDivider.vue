cb643ba1bcc5e0acd5805212d8ae5f889309f99c
qianbin <cola.tin.com@gmail.com>
2020-06-19 23:08:07 +0800

    comp/ScrollDivider: fix unwork in bottom mode

--- src/components/ScrollDivider.vue
+++ src/components/ScrollDivider.vue
@@ -29,11 +29,8 @@ export default Vue.extend({
     computed: {
         opacity(): number {
             if (this.bottom) {
-                if (!this.$el) {
-                    return 0
-                }
-                const parent = this.$el.parentElement!
-                const sHeight = parent.scrollHeight - parent.clientHeight
+                const parent = this.$el ? this.$el.parentElement : null
+                const sHeight = parent ? (parent.scrollHeight - parent.clientHeight) : 0
                 return Math.min(Math.max(sHeight - this.y, 0), 10) / 10
             } else {
                 return Math.min(Math.max(this.y, 0), 10) / 10
