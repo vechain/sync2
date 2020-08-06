c64400a0f63ef06c0d87d770a3c7ce2809014798
qianbin <cola.tin.com@gmail.com>
2020-08-06 16:14:01 +0800

    pages: improve token setting page

--- src/pages/TokensSetting.vue
+++ src/pages/TokensSetting.vue
@@ -3,36 +3,54 @@
         v-scrollDivider
         class="fit overflow-auto"
     >
-        <q-list>
-            <template v-for="(item, index) in tokens">
-                <q-separator
-                    v-if="index !==0 "
-                    :key="item.symbol + 's'"
-                    inset="item"
+        <async
+            :fn="fetchTokenList"
+            v-slot="{pending, reload}"
+        >
+            <q-list v-if="tokens.length>0">
+                <template v-for="(item, index) in tokens">
+                    <q-separator
+                        v-if="index !==0 "
+                        :key="item.symbol + 's'"
+                        inset="item"
+                    />
+                    <q-item :key="item.symbol">
+                        <q-item-section avatar>
+                            <q-avatar
+                                square
+                                size="md"
+                            >
+                                <q-img :src="item.icon" />
+                            </q-avatar>
+                        </q-item-section>
+                        <q-item-section>
+                            <q-item-label lines="1">{{item.symbol}}</q-item-label>
+                            <q-item-label caption>{{item.name}}</q-item-label>
+                        </q-item-section>
+                        <q-item-section side>
+                            <q-toggle
+                                color="green"
+                                v-model="activeTokens"
+                                :val="item.symbol"
+                            />
+                        </q-item-section>
+                    </q-item>
+                </template>
+            </q-list>
+            <template v-else>
+                <q-inner-loading
+                    v-if="pending"
+                    showing
                 />
-                <q-item :key="item.symbol">
-                    <q-item-section avatar>
-                        <q-avatar
-                            square
-                            size="md"
-                        >
-                            <q-img :src="item.icon" />
-                        </q-avatar>
-                    </q-item-section>
-                    <q-item-section>
-                        <q-item-label lines="1">{{item.symbol}}</q-item-label>
-                        <q-item-label caption>{{item.name}}</q-item-label>
-                    </q-item-section>
-                    <q-item-section side>
-                        <q-toggle
-                            color="green"
-                            v-model="activeTokens"
-                            :val="item.symbol"
-                        />
-                    </q-item-section>
-                </q-item>
+                <div
+                    v-else
+                    class="fit column flex-center"
+                >
+                    <p>Something wrong</p>
+                    <q-btn @click="reload">Refresh</q-btn>
+                </div>
             </template>
-        </q-list>
+        </async>
     </div>
 </template>
 <script lang="ts">
@@ -46,7 +64,6 @@ export default Vue.extend({
         }
     },
     created() {
-        this.$state.config.token.fetch()
         this.activeTokens = Array.from(new Set<string>(this.$state.config.token.active))
     },
     async beforeDestroy() {
@@ -58,6 +75,11 @@ export default Vue.extend({
                 ...t,
                 icon: `${urls.tokenRegistry}assets/${t.icon}`
             }))
+        }
+    },
+    methods: {
+        fetchTokenList() {
+            return this.$state.config.token.fetch()
         }
     }
 })
