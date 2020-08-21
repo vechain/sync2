10a7a60403ba3cb28a5611c649cc80e94e2c5fd0
Asbert Ma <stayinalive@msn.cn>
2020-08-21 16:12:34 +0800

    add params get abi query

--- src/components/queries.ts
+++ src/components/queries.ts
@@ -16,3 +16,22 @@ export function tokenBalanceOf(connex: Connex, addr: string, spec: M.TokenSpec):
         .call(addr)
         .then(output => output.decoded!.balance)
 }
+
+const paramsCache: Record<string, string> = {}
+
+export async function getParams(connex: Connex, key: string): Promise<string> {
+    const k = `${connex.thor.genesis.id}-${key}`
+    if (paramsCache[k]) {
+        return paramsCache[k]
+    } else {
+        const address = '0x0000000000000000000000000000506172616d73'
+        const result = await connex.thor
+            .account(address)
+            .method(abis.paramsGet)
+            .cache([address])
+            .call(key)
+
+        paramsCache[k] = result.data
+        return result.data
+    }
+}
