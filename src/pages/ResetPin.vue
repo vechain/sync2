5c318c135b7bc943eaf118528f3a882bee586a6b
Asbert Ma <stayinalive@msn.cn>
2020-09-01 13:11:01 +0800

    reset pin code

---
+++ src/pages/ResetPin.vue
@@ -0,0 +1,72 @@
+<template>
+    <ConfirmedPinCodeInput
+        v-if="show"
+        class="full-height"
+        @fulfilled="handlePin($event)"
+    />
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import { Vault } from '../core/vault'
+export default Vue.extend({
+    data() {
+        return {
+            show: false,
+            oldPin: ''
+        }
+    },
+    async created() {
+        try {
+            this.oldPin = await this.$authenticate((pin) => {
+                return Promise.resolve(pin)
+            })
+            this.show = true
+        } catch (error) {
+            this.$router.back()
+        }
+    },
+    methods: {
+        async handlePin(pin: string) {
+            await this.$loading(
+                async () => {
+                    try {
+                        await this.updateDB(pin)
+                        this.$q.notify({
+                            type: 'positive',
+                            message: 'Master code changed.',
+                            timeout: 1500
+                        })
+                    } catch (error) {
+                        console.log(error)
+                        this.$q.notify({
+                            type: 'warning',
+                            message: 'Something wrong!',
+                            timeout: 1500
+                        })
+                    } finally {
+                        this.$router.back()
+                    }
+                }
+            )
+        },
+        async updateDB(pin: string) {
+            await this.$storage.transaction(async () => {
+                const wallets = await this.$storage.wallets.all().query()
+                const shadow = await this.$storage.waitFor(Vault.shadowPassword(pin))
+                await this.$storage.configs.update({ key: 'passwordShadow' }, {
+                    value: shadow
+                })
+                for (let i = 0; i < wallets.length; i++) {
+                    const w = wallets[i]
+                    const vault = await this.$storage.waitFor(Vault.decode(w.vault))
+                    const words = await this.$storage.waitFor(vault.decrypt(this.oldPin)) as string
+                    const newVault = await this.$storage.waitFor(Vault.createHD(words.split(' '), pin))
+                    await this.$storage.wallets.update({ id: w.id }, {
+                        vault: newVault.encode()
+                    })
+                }
+            })
+        }
+    }
+})
+</script>
