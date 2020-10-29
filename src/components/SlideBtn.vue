4626e0ee743f8dc990a35dfdfbe066c5008cddca
Asbert Ma <stayinalive@msn.cn>
2020-10-29 14:14:29 +0800

    bug fix, event & status

--- src/components/SlideBtn.vue
+++ src/components/SlideBtn.vue
@@ -6,31 +6,26 @@
         :style="{height: cHeight, borderRadius: cHeight}"
     >
         <div
-            :class="[{'trs': isTrans}, rClass]"
+            :class="rClass"
             class="absolute row items-center justify-center full-width full-height"
         >
             <div
-                :class="textClass"
+                :class="[textClass, {'trs': isTrans}]"
                 :style="{opacity: opacity}"
             >
                 {{label}}
             </div>
         </div>
-        <div
-            :class="[{'trs': isTrans}, lClass]"
-            @transitionend="isTrans = false"
-            class="full-width full-height absolute"
-            :style="{right: right, borderRadius: cHeight}"></div>
         <q-btn
             ref="btn"
             :color="btnColor"
             unelevated
-            class="absolute"
+            @transitionend="onTransitionend"
             :class="{'trs': isTrans}"
             round
             :disabled="isTrans || disabled"
             v-touch-pan.prevent.right.mouse="moveFab"
-            :style="{left: left}"
+            :style="`transform: translateX(${left})`"
             :icon="btnIcon"
         />
     </div>
@@ -45,7 +40,6 @@ export default Vue.extend({
     props: {
         label: String,
         btnIcon: { type: String, default: 'keyboard_arrow_right' },
-        lClass: { type: String, default: 'bg-grey-2' },
         rClass: { type: String, default: 'bg-grey-3' },
         btnColor: { type: String, default: 'primary' },
         textClass: { type: String, default: 'text-grey-9' },
@@ -57,24 +51,16 @@ export default Vue.extend({
             XOffset: 0,
             maxOffset: 0,
             height: 0,
+            opacity: 1,
             isTrans: false
         }
     },
     mounted() {
-        const c = this.$refs.container as HTMLDivElement
-        const b = this.$refs.btn as Vue
-        this.height = b.$el.clientHeight
-        this.maxOffset = c.offsetWidth - b.$el.clientWidth
+        this.height = (this.$refs.btn as Vue).$el.clientHeight
     },
     computed: {
         left(): string {
             return `${this.XOffset}px`
-        },
-        right(): string {
-            return `${this.maxOffset - this.XOffset}px`
-        },
-        opacity(): number {
-            return this.XOffset === this.maxOffset ? 0 : (1 - this.XOffset / this.maxOffset)
         },
         cHeight(): string {
             return `${this.height}px`
@@ -84,36 +70,45 @@ export default Vue.extend({
         checked(val: boolean) {
             if (!val) {
                 this.XOffset = 0
+                this.opacity = 1
             }
         }
     },
     methods: {
+        onTransitionend() {
+            if (this.isTrans) {
+                this.$emit('checked', true)
+                this.isTrans = false
+            }
+        },
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         moveFab(details: any) {
+            const maxOffset = (this.$refs.container as HTMLDivElement).offsetWidth - (this.$refs.btn as Vue).$el.clientWidth
             if (this.disabled) {
                 return
             }
-            if (details.distance.x > this.maxOffset) {
-                this.XOffset = this.maxOffset
+            if (details.distance.x > maxOffset) {
+                this.XOffset = maxOffset
             } else {
                 this.XOffset = details.distance.x
             }
 
             if (details.isFinal) {
-                this.isTrans = true
-                if (this.XOffset > this.maxOffset * 0.85) {
-                    this.XOffset = this.maxOffset
-                    this.$emit('checked', true)
+                if (this.XOffset > maxOffset * 0.85) {
+                    this.isTrans = true
+                    this.XOffset = maxOffset
                 } else {
                     this.XOffset = 0
                 }
             }
+
+            this.opacity = this.XOffset === maxOffset ? 0 : (1 - this.XOffset / maxOffset)
         }
     }
 })
 </script>
 <style scoped>
 .trs {
-    transition: left 0.4s ease-out, right 0.4s ease-out, opacity 0.4s ease-out;
+    transition: transform 0.1s ease-out, opacity 0.1s ease-out;
 }
 </style>
