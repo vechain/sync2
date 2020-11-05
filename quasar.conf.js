7dc3a37fdb21e8db36ac7e3e4c389a92a7a4f2a6
qianbin <cola.tin.com@gmail.com>
2020-11-06 00:26:50 +0800

    config: add workbox options

--- quasar.conf.js
+++ quasar.conf.js
@@ -150,7 +150,10 @@ module.exports = configure(function (/* ctx */) {
     // https://quasar.dev/quasar-cli/developing-pwa/configuring-pwa
     pwa: {
       workboxPluginMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
-      workboxOptions: {}, // only for GenerateSW
+      workboxOptions: {
+        skipWaiting: true,
+        clientsClaim: true
+      }, // only for GenerateSW
       manifest: {
         name: 'Sync',
         // eslint-disable-next-line @typescript-eslint/camelcase
