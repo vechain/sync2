e749c735bdb57b85e68b7802fcb9ea9076216c0d
Asbert Ma <stayinalive@msn.cn>
2020-11-06 11:21:01 +0800

    set pwd: validate behavior changed

--- src/components/InputPinCode.vue
+++ src/components/InputPinCode.vue
@@ -6,36 +6,27 @@
             debounce="500"
             autocomplete="off"
             v-model="firstOne"
+            placeholder="Passsword at least 6 characters"
             :rules="[
                 val => !!val || 'Please enter the new password',
                 val => val.length > 5 || 'Require at least 6 characters'
             ]"
         />
+        <!-- @focus="onFocus" -->
         <q-input
             label="Confirm Password"
             type="password"
-            debounce="500"
+            ref="secondOne"
+            lazy-rules="ondemand"
             autocomplete="off"
-            @input="(val) => $emit('change', val)"
+            @input="onChange"
             v-model="secondOne"
             :rules="[codeMatch]"
         />
-        <div>
-            <div class="q-mb-sm text-caption">Password requirements:</div>
-            <div
-                :class="firstOne.length > 5 ? 'text-blue-7' : 'text-grey-7'"
-                class="row items-center q-gutter-sm"
-            >
-                <q-icon name="check_circle" />
-                <span class="row">
-                    At least 6 characters
-                </span>
-
-            </div>
-        </div>
     </div>
 </template>
 <script lang="ts">
+import { QInput } from 'quasar'
 import Vue from 'vue'
 export default Vue.extend({
     model: {
@@ -48,12 +39,23 @@ export default Vue.extend({
     data() {
         return {
             firstOne: '',
-            secondOne: ''
+            secondOne: '',
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            timer: null as any
         }
     },
     methods: {
         codeMatch(val: string): boolean | string {
             return val === this.firstOne || 'Password mismatch'
+        },
+        onChange(val: string) {
+            this.$emit('change', val)
+            if (this.timer) {
+                clearTimeout(this.timer)
+            }
+            this.timer = setTimeout(() => {
+                (this.$refs.secondOne as QInput).resetValidation()
+            }, 200)
         }
     }
 })
