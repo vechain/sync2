46e7a35a90bcb890fcd9b39dd43c2f924faa64f2
Asbert Ma <stayinalive@msn.cn>
2020-11-06 11:22:40 +0800

    set pwd & activities: words changed

--- src/pages/ResetPin.vue
+++ src/pages/ResetPin.vue
@@ -4,7 +4,7 @@
         class="q-pt-md q-px-md"
     >
         <div class="text-subtitle2">
-            Password allows you to access Sync2 and unlock your wallet. If you forget the password, you will NOT be able to access Sync2. You will need to delete the app and restore the wallet with mnemonic words.
+            Password allows you to access Sync2 and unlock your wallet. If you forget the password, you will NOT be able to access Sync2.
         </div>
         <q-form @submit="onSubmit">
             <InputPinCode v-model="code" />
