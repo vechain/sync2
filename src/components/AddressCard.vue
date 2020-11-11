8d12fa8c89bf9e52bb77274eb5c0baa532792630
qianbin <cola.tin.com@gmail.com>
2020-11-11 11:12:59 +0800

    comp/AddressCard: adjust card background & cache svg icon

--- src/components/AddressCard.vue
+++ src/components/AddressCard.vue
@@ -44,6 +44,38 @@ import Vue from 'vue'
 import { picasso } from '@vechain/picasso'
 import Color from 'color'
 
+/**
+ * it rasterize svg into bitmap compressed in png format
+ * @param svg the svg string
+ * @returns url of bitmap
+ */
+function rasterize(svg: string) {
+    const size = 200
+    return new Promise<string>((resolve, reject) => {
+        const img = new Image()
+        img.onload = () => {
+            const canvas = document.createElement('canvas')
+            canvas.setAttribute('width', size.toString())
+            canvas.setAttribute('height', size.toString())
+            const ctx = canvas.getContext('2d')
+            if (!ctx) {
+                return reject(new Error('failed to get 2d context'))
+            }
+            ctx.drawImage(img, 0, 0, size, size)
+            // ctx.globalCompositeOperation = 'soft-light'
+            // ctx.fillStyle = '#666'
+            // ctx.fillRect(0, 0, size, size)
+            canvas.toBlob(r => {
+                resolve(URL.createObjectURL(r))
+            })
+        }
+        img.onerror = err => reject(err)
+        img.src = `data:image/svg+xml;utf8,${svg}`
+    })
+}
+
+const imageCache = new Map<string, Promise<string>>() // mapping address => imageurl
+
 export default Vue.extend({
     props: {
         address: String,
@@ -52,39 +84,59 @@ export default Vue.extend({
     },
     data: () => {
         return {
+            background: undefined as unknown as object,
+            iconStyles: undefined as unknown as object, // type hacks for async-computed
             height: 0
         }
     },
     computed: {
         svg(): string {
             return picasso(this.address)
+        }
+    },
+    asyncComputed: {
+        async iconStyles(): Promise<object> {
+            const img = await this.generateIconImg()
+            return {
+                background: `url('${img}') center / cover no-repeat`,
+                width: '4rem',
+                height: '4rem',
+                borderRadius: '50%'
+            }
         },
-        background(): object {
+        async background(): Promise<object> {
+            await this.generateIconImg() // to prevent background rendered before icon
+
             let str = this.svg
             let color!: Color
             for (let i = 0; i < 2; i++) {
                 const m = /rgb\((\d+),\s*(\d+),\s*(\d+)\)/.exec(str)
                 if (m) {
-                    color = color ? color.mix(Color(m[0]), 0.25) : Color(m[0])
+                    color = color ? color.mix(Color(m[0]), 0.4) : Color(m[0])
                     str = str.slice(m.index + m[0].length)
                 } else {
                     break
                 }
             }
+
+            const c1 = color.saturationl(30).lightness(30)
+            const c2 = color.saturationl(30).lightness(45)
             return {
-                background: `linear-gradient(to bottom, ${color.darken(0.6).rgb().string()}, ${color.rgb().string()})`
-            }
-        },
-        iconStyles(): object {
-            return {
-                background: `url('data:image/svg+xml;utf8,${this.svg}') center / cover no-repeat`,
-                width: '4rem',
-                height: '4rem',
-                borderRadius: '50%'
+                background: `linear-gradient(to bottom, ${c1.rgb().string()}, ${c2.rgb().string()})`
             }
         }
     },
     methods: {
+        generateIconImg() {
+            const addr = this.address
+            let img = imageCache.get(addr)
+            if (!img) {
+                const svg = picasso(addr)
+                img = rasterize(svg)
+                imageCache.set(addr, img)
+            }
+            return img
+        },
         onResize(size: { height: number }) {
             if (size.height > 0) {
                 this.height = size.height
