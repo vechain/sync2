c2d60f1c3c04208c9b8b68f91dbf9de003501b70
qianbin <cola.tin.com@gmail.com>
2020-12-02 16:05:46 +0800

    improve types for vue-async-computed

---
+++ src/shims-vue-async-computed.d.ts
@@ -0,0 +1,33 @@
+import { ComponentOptions, DataDef, RecordPropsDefinition } from 'vue/types/options'
+import { AsyncComputedGetter, IAsyncComputedValue, IASyncComputedState } from 'vue-async-computed'
+
+export type AsyncComputedObject<T> = {
+    [K in keyof T]: AsyncComputedGetter<T[K]> | IAsyncComputedValue<T[K]>;
+}
+
+export type AsyncComputedStates<T> = {
+    $asyncComputed: { [K in keyof T]: IASyncComputedState };
+}
+
+
+export interface AsyncComputedOption<T> {
+    asyncComputed?: AsyncComputedObject<T>;
+}
+
+declare module 'vue/types/vue' {
+    interface VueConstructor<V extends Vue = Vue> {
+        extend<Data, Methods, Computed, PropNames extends string, AsyncComputed>(options?:
+            object &
+            ComponentOptions<V, DataDef<Data, Record<PropNames, any>, V>, Methods, Computed, PropNames[], Record<PropNames, any>> &
+            AsyncComputedOption<AsyncComputed> &
+            ThisType<CombinedVueInstance<V, Data, Methods, Computed & AsyncComputed & AsyncComputedStates<AsyncComputed>, Readonly<Record<PropNames, any>>>>
+        ): ExtendedVue<V, Data, Methods, Computed & AsyncComputed & AsyncComputedStates<AsyncComputed>, Record<PropNames, any>>;
+
+        extend<Data, Methods, Computed, Props, AsyncComputed>(options?:
+            object &
+            ComponentOptions<V, DataDef<Data, Props, V>, Methods, Computed, RecordPropsDefinition<Props>, Props> &
+            AsyncComputedOption<AsyncComputed> &
+            ThisType<CombinedVueInstance<V, Data, Methods, Computed & AsyncComputed & AsyncComputedStates<AsyncComputed>, Readonly<Props>>>
+        ): ExtendedVue<V, Data, Methods, Computed & AsyncComputed & AsyncComputedStates<AsyncComputed>, Props>;
+    }
+}
