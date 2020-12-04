3aa29e0319aa60a43e373428b38e2ecffb90d1a1
qianbin <cola.tin.com@gmail.com>
2020-12-04 17:06:15 +0800

    boot/services/blockchain: add balanceOf method

--- src/boot/services/blockchain.ts
+++ src/boot/services/blockchain.ts
@@ -1,9 +1,25 @@
 import { createThorFactory } from './utils'
+import { abis } from 'src/consts'
 
 function serve(exec: ReturnType<typeof createThorFactory>) {
     return {
         getAccount(addr: string) {
             return exec(thor => thor.account(addr).get())
+        },
+        balanceOf(addr: string, spec: M.TokenSpec | 'vet' | 'vtho') {
+            return exec(thor => {
+                if (spec === 'vet') {
+                    return thor.account(addr).get().then(a => a.balance)
+                } else if (spec === 'vtho') {
+                    return thor.account(addr).get().then(a => a.energy)
+                } else {
+                    return thor.account(spec.address)
+                        .method(abis.balanceOf)
+                        .cache([addr])
+                        .call(addr)
+                        .then(output => output.decoded.balance)
+                }
+            })
         }
     }
 }
