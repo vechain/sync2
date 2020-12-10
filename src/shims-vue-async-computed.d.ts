a4ba0683b0a35b957991eb43a12ce769b057edc7
qianbin <cola.tin.com@gmail.com>
2020-12-10 23:51:01 +0800

    improve shims for vue-async-computed

--- src/shims-vue-async-computed.d.ts
+++ src/shims-vue-async-computed.d.ts
@@ -16,14 +16,14 @@ export interface AsyncComputedOption<T> {
 
 declare module 'vue/types/vue' {
     interface VueConstructor<V extends Vue = Vue> {
-        extend<Data, Methods, Computed, PropNames extends string, AsyncComputed>(options?:
+        extend<Data, Methods, Computed, PropNames extends string = never, AsyncComputed = {}>(options?:
             object &
             ComponentOptions<V, DataDef<Data, Record<PropNames, any>, V>, Methods, Computed, PropNames[], Record<PropNames, any>> &
             AsyncComputedOption<AsyncComputed> &
             ThisType<CombinedVueInstance<V, Data, Methods, Computed & AsyncComputed & AsyncComputedStates<AsyncComputed>, Readonly<Record<PropNames, any>>>>
         ): ExtendedVue<V, Data, Methods, Computed & AsyncComputed & AsyncComputedStates<AsyncComputed>, Record<PropNames, any>>;
 
-        extend<Data, Methods, Computed, Props, AsyncComputed>(options?:
+        extend<Data, Methods, Computed, Props, AsyncComputed = {}>(options?:
             object &
             ComponentOptions<V, DataDef<Data, Props, V>, Methods, Computed, RecordPropsDefinition<Props>, Props> &
             AsyncComputedOption<AsyncComputed> &
