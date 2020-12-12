30baa4795ca2e1367cba8d8e3f5cc0334211045d
qianbin <cola.tin.com@gmail.com>
2020-12-12 17:32:55 +0800

    utils/array: add unique method

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,24 +1,48 @@
 export function groupBy<T, U>(array: T[], by: (v: T) => U): T[][] {
-    const ret: T[][] = []
+    if (array.length <= 10) {
+        return array.reduce<T[][]>((prev, cur) => {
+            const k = by(cur)
+            const f = prev.find(e => by(e[0]) === k)
+            f ? f.push(cur) : prev.push([cur])
+            return prev
+        }, [])
+    }
+
     const map = new Map<U, T[]>()
-    array.forEach(v => {
-        const k = by(v)
-        const exist = map.get(k)
-        if (exist) {
-            exist.push(v)
+    return array.reduce<T[][]>((prev, cur) => {
+        const k = by(cur)
+        const f = map.get(k)
+        if (f) {
+            f.push(cur)
         } else {
-            const sub = [v]
-            ret.push(sub)
-            map.set(k, sub)
+            const g = [cur]
+            prev.push(g)
+            map.set(k, g)
         }
-    })
-    return ret
+        return prev
+    }, [])
 }
 
 export function count<T>(array: T[], cond: (v: T) => boolean): number {
-    let n = 0
-    array.forEach(v => {
-        cond(v) && n++
-    })
-    return n
+    return array.reduce((prev, cur) => {
+        cond(cur) && prev++
+        return prev
+    }, 0)
+}
+
+export function unique<T>(array: T[]): T[] {
+    if (array.length <= 10) {
+        return array.reduce<T[]>((prev, cur) => {
+            prev.includes(cur) || prev.push(cur)
+            return prev
+        }, [])
+    }
+    const set = new Set<T>()
+    return array.reduce<T[]>((prev, cur) => {
+        if (!set.has(cur)) {
+            set.add(cur)
+            prev.push(cur)
+        }
+        return prev
+    }, [])
 }
