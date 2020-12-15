4bd2cd8989b43800d4944a5a8069ed5e76663941
qianbin <cola.tin.com@gmail.com>
2020-12-15 14:14:43 +0800

    boot/services: chagnes due to model change

--- src/boot/services/activity.ts
+++ src/boot/services/activity.ts
@@ -2,11 +2,12 @@ import { Storage } from 'core/storage'
 import { delegateTable } from './utils'
 
 export function build(storage: Storage) {
-    const t = delegateTable<Storage.ActivityEntity, M.Activity<'tx' | 'cert'>>(
+    const t = delegateTable<Storage.ActivityEntity, M.Activity>(
         storage.activities,
         e => ({
             id: e.id,
             gid: e.gid,
+            type: e.type,
             walletId: e.walletId,
             createdTime: e.createdTime,
             status: e.status,
@@ -15,6 +16,7 @@ export function build(storage: Storage) {
         m => ({
             id: m.id,
             gid: m.gid,
+            type: m.type,
             walletId: m.walletId,
             createdTime: m.createdTime,
             status: m.status,
