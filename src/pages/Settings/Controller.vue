fbc2a632b7dae1ceee6c86ec0dbf17c49142e5a0
qianbin <cola.tin.com@gmail.com>
2020-12-17 14:47:03 +0800

    pages/Settings: adjust language selector again

--- src/pages/Settings/Controller.vue
+++ src/pages/Settings/Controller.vue
@@ -13,24 +13,26 @@
                 :value="languageDisplayName($i18n.locale)"
                 clickable
             />
-            <q-menu
+            <q-popup-proxy
                 target="#lang"
                 anchor="bottom right"
                 self="top right"
                 :offset="[-16,0]"
             >
-                <q-list separator>
-                    <q-item
-                        clickable
-                        v-close-popup
-                        v-for="lang in $i18n.availableLocales"
-                        :key="lang"
-                        @click="setLanguage(lang)"
-                    >
-                        <q-item-section>{{languageDisplayName(lang)}}</q-item-section>
-                    </q-item>
-                </q-list>
-            </q-menu>
+                <q-card>
+                    <q-list separator>
+                        <q-item
+                            clickable
+                            v-close-popup
+                            v-for="lang in $i18n.availableLocales"
+                            :key="lang"
+                            @click="setLanguage(lang)"
+                        >
+                            <q-item-section class="q-px-lg text-center">{{languageDisplayName(lang)}}</q-item-section>
+                        </q-item>
+                    </q-list>
+                </q-card>
+            </q-popup-proxy>
             <q-separator inset="item" />
             <item
                 icon="mdi-lock"
