fdb2f195b2a7eacb56af569145b6bb68cca7bb7b
qianbin <cola.tin.com@gmail.com>
2020-12-19 02:05:39 +0800

    boot: reorg misc module -- extract modals parts

--- src/boot/misc/index.ts
+++ src/boot/misc/index.ts
@@ -1,12 +1,7 @@
 import { boot } from 'quasar/wrappers'
 import * as State from './state'
 import * as Plugins from './plugins'
-
-import AuthenticationDialog from 'pages/AuthenticationDialog'
-import { QSpinnerIos, DialogChainObject, QDialogOptions } from 'quasar'
-import ActionSheets from 'pages/ActionSheets.vue'
-import TxSigningDialog from 'pages/TxSigningDialog'
-import CertSigningDialog from 'pages/CertSigningDialog.vue'
+import * as Modals from './modals'
 import { genesises } from 'src/consts'
 
 declare module 'vue/types/vue' {
@@ -15,93 +10,15 @@ declare module 'vue/types/vue' {
         $backOrHome(): void
         /** returns the display name of network identified by gid */
         $netDisplayName(gid: string): string
-        /**
-         * pop up the authentication dialog to ask user entering password
-         * @returns verified user password
-         */
-        $authenticate(): Promise<string>
-
-        /**
-         * protected the async task with a loading mask
-         * @param task the async task
-         * @returns the result of the task
-         */
-        $loading<T>(task: () => Promise<T>): Promise<T>
-
-        /** display an action sheets */
-        $actionSheets(actions: Array<{ label: string, classes?: string | string[], onClick?: Function }>): Promise<boolean>
-
-        /**
-         * sign tx
-         * @param gid desired genesis id of user wallet
-         * @param req request content
-         */
-        $signTx(
-            gid: string,
-            req: M.TxRequest
-        ): Promise<M.TxResponse>
-
-        /**
-         * sign cert
-         * @param gid desired genesis id of user wallet
-         * @param req request content
-         */
-        $signCert(
-            gid: string,
-            req: M.CertRequest
-        ): Promise<M.CertResponse>
-
         /** it wraps window.addEventListener binding to vue component's life-cycle */
         $onWindowEvent(event: keyof WindowEventMap, listener: EventListenerOrEventListenerObject): void
     }
 }
 
-const replaceDialog = (() => {
-    let prev: DialogChainObject | undefined
-    return <T>(vm: Vue, options: QDialogOptions) => {
-        return new Promise<T>((resolve, reject) => {
-            // close the previous dialog if one opened
-            if (prev) {
-                prev.hide()
-                prev = undefined
-            }
-            const cur = vm.$q.dialog(options)
-            cur.onOk(resolve)
-                .onCancel((e: Error) => reject(e || new Error('cancelled')))
-                .onDismiss(() => {
-                    if (prev === cur) {
-                        prev = undefined
-                    }
-                })
-
-            prev = cur
-        })
-    }
-})()
-
 export default boot(({ Vue }) => {
     State.boot()
     Plugins.boot()
-
-    let loadingCount = 0
-
-    const delayedSpinner = Vue.component('DelayedSpinner', {
-        data: () => { return { display: false } },
-        props: { color: String, size: Number },
-        created() { setTimeout(() => { this.display = true }, 200) },
-        render(h) {
-            if (!this.display) {
-                return h()
-            }
-            const spinner = h(QSpinnerIos, { props: this.$props })
-            return h('transition', {
-                props: {
-                    name: 'q-transition--fade',
-                    appear: true
-                }
-            }, [spinner])
-        }
-    })
+    Modals.boot()
 
     Object.defineProperties(Vue.prototype, {
         $backOrHome: {
@@ -127,90 +44,6 @@ export default boot(({ Vue }) => {
                             return name + suffix
                         }
                     }
-                }
-            }
-        },
-        $authenticate: {
-            get(): Vue['$authenticate'] {
-                const vm = this as Vue
-                return () => {
-                    return new Promise((resolve, reject) => {
-                        vm.$q.dialog({
-                            component: AuthenticationDialog,
-                            parent: vm
-                        })
-                            .onOk(resolve)
-                            .onCancel(() => reject(new Error('cancelled')))
-                    })
-                }
-            }
-        },
-        $loading: {
-            get(): Vue['$loading'] {
-                const root = (this as Vue).$root
-                return async (task) => {
-                    try {
-                        if (loadingCount++ === 0) {
-                            // set 0 delay to block mouse/touch event
-                            root.$q.loading.show({
-                                spinner: delayedSpinner as unknown as Vue,
-                                delay: 0,
-                                backgroundColor: 'transparent',
-                                spinnerColor: 'black'
-                            })
-                        }
-                        return await task()
-                    } finally {
-                        if (--loadingCount === 0) {
-                            root.$q.loading.hide()
-                        }
-                    }
-                }
-            }
-        },
-        $actionSheets: {
-            get(): Vue['$actionSheets'] {
-                const vm = this as Vue
-                return actions => {
-                    return new Promise((resolve) => {
-                        vm.$q.dialog({
-                            component: ActionSheets,
-                            parent: vm,
-                            actions
-                        }).onCancel(() => {
-                            resolve(false)
-                        }).onDismiss(() => {
-                            resolve(false)
-                        }).onOk(() => {
-                            resolve(true)
-                        })
-                    })
-                }
-            }
-        },
-        $signTx: {
-            get(): Vue['$signTx'] {
-                const vm = this as Vue
-                return (gid, req) => {
-                    return replaceDialog(vm, {
-                        component: TxSigningDialog,
-                        parent: vm,
-                        gid,
-                        req
-                    })
-                }
-            }
-        },
-        $signCert: {
-            get(): Vue['$signCert'] {
-                const vm = this as Vue
-                return (gid, req) => {
-                    return replaceDialog(vm, {
-                        component: CertSigningDialog,
-                        parent: vm,
-                        gid,
-                        req
-                    })
                 }
             }
         },
---
+++ src/boot/misc/modals.ts
@@ -0,0 +1,147 @@
+import Vue from 'vue'
+import AuthenticationDialog from 'pages/AuthenticationDialog'
+import ActionSheets from 'components/ActionSheets.vue'
+import TxSigningDialog from 'pages/TxSigningDialog'
+import CertSigningDialog from 'pages/CertSigningDialog.vue'
+import { QSpinnerIos } from 'quasar'
+
+declare module 'vue/types/vue' {
+    interface Vue {
+        /**
+         * pop up the authentication dialog to ask user entering password
+         * @returns verified user password
+         */
+        $authenticate(): Promise<string>
+
+        /**
+         * protected the async task with a loading mask
+         * @param task the async task
+         * @returns the result of the task
+         */
+        $loading<T>(task: () => Promise<T>): Promise<T>
+
+        /** display an action sheets */
+        $actionSheets(actions: Array<{ label: string, classes?: string | string[], onClick?: Function }>): Promise<boolean>
+
+        /**
+         * sign tx
+         * @param gid desired genesis id of user wallet
+         * @param req request content
+         */
+        $signTx(
+            gid: string,
+            req: M.TxRequest
+        ): Promise<M.TxResponse>
+
+        /**
+         * sign cert
+         * @param gid desired genesis id of user wallet
+         * @param req request content
+         */
+        $signCert(
+            gid: string,
+            req: M.CertRequest
+        ): Promise<M.CertResponse>
+    }
+}
+
+const loadingFunc = (() => {
+    let counter = 0
+    return async <T>(vm: Vue, task: () => Promise<T>) => {
+        let delayTimer
+        try {
+            if (counter++ === 0) {
+                // set 0 delay to block mouse/touch event
+                vm.$q.loading.show({
+                    spinner: undefined,
+                    delay: 0,
+                    backgroundColor: 'transparent'
+                })
+                delayTimer = setTimeout(() => {
+                    vm.$q.loading.show({
+                        spinner: QSpinnerIos as unknown as Vue,
+                        delay: 0,
+                        backgroundColor: 'transparent',
+                        spinnerColor: 'black'
+                    })
+                }, 200)
+            }
+            return await task()
+        } finally {
+            if (--counter === 0) {
+                if (delayTimer) {
+                    clearTimeout(delayTimer)
+                }
+                vm.$q.loading.hide()
+            }
+        }
+    }
+})()
+
+function dialog<T>(vm: Vue, options: object) {
+    return new Promise<T>((resolve, reject) => {
+        vm.$q.dialog({ ...options, parent: vm /* always set parent */ })
+            .onOk(resolve)
+            .onCancel((e: Error) => reject(e || new Error('cancelled')))
+    })
+}
+
+export function boot() {
+    Object.defineProperties(Vue.prototype, {
+        $loading: {
+            get(): Vue['$loading'] {
+                const root = (this as Vue).$root
+                return async (task) => {
+                    return loadingFunc(root, task)
+                }
+            }
+        },
+        $authenticate: {
+            get(): Vue['$authenticate'] {
+                const vm = this as Vue
+                return () => {
+                    return dialog(vm, {
+                        component: AuthenticationDialog
+                    })
+                }
+            }
+        },
+        $actionSheets: {
+            get(): Vue['$actionSheets'] {
+                const vm = this as Vue
+                return actions => {
+                    return dialog(vm, {
+                        component: ActionSheets,
+                        actions
+                    }).then(() => true)
+                        .catch(() => false)
+                }
+            }
+        },
+
+        $signTx: {
+            get(): Vue['$signTx'] {
+                const vm = this as Vue
+                return (gid, req) => {
+                    return dialog(vm, {
+                        component: TxSigningDialog,
+                        gid,
+                        req
+                    })
+                }
+            }
+        },
+        $signCert: {
+            get(): Vue['$signCert'] {
+                const vm = this as Vue
+                return (gid, req) => {
+                    return dialog(vm, {
+                        component: CertSigningDialog,
+                        gid,
+                        req
+                    })
+                }
+            }
+        }
+    })
+}
