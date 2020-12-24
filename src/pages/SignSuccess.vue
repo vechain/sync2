ab13a763db360c571e7e7a17bd7fff515e4135ef
qianbin <cola.tin.com@gmail.com>
2020-12-24 13:54:10 +0800

    pages/SignSuccess: tweak layout

--- src/pages/SignSuccess.vue
+++ src/pages/SignSuccess.vue
@@ -1,29 +1,29 @@
 <template>
     <div class="column fit">
         <page-toolbar title="Success" />
-        <div class="narrow-page col column self-center items-center q-pa-md no-wrap q-gutter-y-sm">
-            <div class="q-my-auto text-center">
-                <p>
-                    <q-icon
-                        size="4rem"
-                        name="check_circle_outline"
-                        color="positive"
-                    />
-                </p>
-                <p class="text-h6">You've signed the {{typeText}}</p>
-            </div>
+        <div class="col q-pa-md column flex-center">
+            <p>
+                <q-icon
+                    size="4rem"
+                    name="check_circle_outline"
+                    color="positive"
+                />
+            </p>
+            <p class="text-h6">You've signed the {{typeText}}</p>
+        </div>
+        <div class="narrow-page row justify-around q-mx-auto q-pa-sm">
+            <q-btn
+                outline
+                color="primary"
+                class="w40"
+                :to="{name:'activities'}"
+            >View Activities</q-btn>
             <q-btn
                 unelevated
                 color="primary"
                 class="w40"
                 @click="close()"
             >Close</q-btn>
-            <q-btn
-                flat
-                color="primary"
-                class="w40"
-                :to="{name:'activities'}"
-            >View Activities</q-btn>
         </div>
     </div>
 </template>
