511aea8b56ce5e05030c64867c4e903c74486df6
qianbin <cola.tin.com@gmail.com>
2020-12-24 10:43:16 +0800

    tidy routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,18 +1,17 @@
 import { RouteConfig } from 'vue-router'
 import Main from 'layouts/Main'
 import Index from 'pages/Index'
+import Address from 'pages/Address'
+import Asset from 'pages/Asset'
+import Backup from 'pages/Backup'
 import NewWallet from 'pages/NewWallet'
+import Send from 'pages/Send'
 import Settings from 'pages/Settings'
 import TokensSetting from 'pages/TokensSetting.vue'
-import Backup from 'pages/Backup'
-import Address from 'pages/Address'
-import Activities from 'pages/Activities'
-import ResetPin from 'pages/ResetPin.vue'
-import Asset from 'pages/Asset'
-import Send from 'pages/Send'
+import NodesSetting from 'pages/NodesSetting'
 import Sign from 'pages/Sign'
 import SignSuccess from 'pages/SignSuccess.vue'
-import NodesSetting from 'src/pages/NodesSetting'
+import Activities from 'pages/Activities'
 
 const routes: RouteConfig[] = [
     {
@@ -31,9 +30,17 @@ const routes: RouteConfig[] = [
             name: 'asset',
             component: Asset
         }, {
+            path: 'w/:walletId(\\d+)/backup',
+            name: 'backup',
+            component: Backup
+        }, {
             path: 'new-wallet',
             name: 'new-wallet',
             component: NewWallet
+        }, {
+            path: 'send',
+            name: 'send',
+            component: Send
         }, {
             path: 'settings',
             name: 'settings',
@@ -43,34 +50,22 @@ const routes: RouteConfig[] = [
             name: 'tokens-setting',
             component: TokensSetting
         }, {
-            path: 'w/:walletId(\\d+)/backup',
-            name: 'backup',
-            component: Backup
+            path: 'nodes-setting',
+            name: 'nodes-setting',
+            component: NodesSetting
         }, {
             // this entry is to handle external signing request in SPA mode only
             path: 'sign',
             name: 'sign',
             component: Sign
         }, {
-            path: 'activities',
-            name: 'activities',
-            component: Activities
-        }, {
-            path: 'reset-pin-code',
-            name: 'reset-pin-code',
-            component: ResetPin
-        }, {
-            path: 'send',
-            name: 'send',
-            component: Send
-        }, {
             path: 'sign-success',
             name: 'sign-success',
             component: SignSuccess
         }, {
-            path: 'nodes-setting',
-            name: 'nodes-setting',
-            component: NodesSetting
+            path: 'activities',
+            name: 'activities',
+            component: Activities
         }]
     }
 ]
@@ -79,7 +74,7 @@ const routes: RouteConfig[] = [
 if (process.env.MODE !== 'ssr') {
     routes.push({
         path: '*',
-        component: () => import('pages/Error404.vue')
+        redirect: '/'
     })
 }
 
