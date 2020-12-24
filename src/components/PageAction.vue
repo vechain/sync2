852b48a4d7b36715411f1556ae2145eb1134c470
qianbin <cola.tin.com@gmail.com>
2020-12-24 21:46:51 +0800

    comp: add PageAction

---
+++ src/components/PageAction.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="q-pa-sm">
+        <div class="bar narrow-page q-mx-auto row justify-around">
+            <slot />
+        </div>
+    </div>
+</template>
+<style scoped>
+.bar > * {
+    width: 42%;
+}
+</style>
