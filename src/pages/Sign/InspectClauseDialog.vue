7359375c2ad8b55210363c0f31dd03e6081d00e2
qianbin <cola.tin.com@gmail.com>
2021-01-08 10:55:35 +0800

    pags/Sign: improve InspectClauseDialog

--- src/pages/Sign/InspectClauseDialog.vue
+++ src/pages/Sign/InspectClauseDialog.vue
@@ -8,49 +8,55 @@
             <q-toolbar>
                 <q-toolbar-title class="text-center">Clause · {{index+1}}</q-toolbar-title>
             </q-toolbar>
-            <q-item>
-                <q-item-section>
-                    <q-item-label caption>To</q-item-label>
-                    <q-item-label style="word-break:break-all">
-                        <address-label
-                            :addr="clause.to"
-                            full
-                        >null</address-label>
-                    </q-item-label>
-                </q-item-section>
-            </q-item>
-            <q-item>
-                <q-item-section>
-                    <q-item-label caption>Value</q-item-label>
-                    <q-item-label>
-                        <amount-label
-                            :value="clause.value"
-                            :fixed="0"
-                        />
-                    </q-item-label>
-                </q-item-section>
-            </q-item>
-            <q-item>
-                <q-item-section>
-                    <q-item-label caption>Data</q-item-label>
-                    <q-item-label>
-                        <q-input
-                            dense
-                            class="monospace"
-                            type="textarea"
-                            standout
-                            readonly
-                            :value="clause.data || '0x'"
-                        />
-                    </q-item-label>
-                </q-item-section>
-            </q-item>
-            <q-item>
-                <q-item-section>
-                    <q-item-label caption>Comment</q-item-label>
-                    <q-item-label class="text-italic">{{clause.comment || 'No comment'}}</q-item-label>
-                </q-item-section>
-            </q-item>
+            <q-list padding>
+                <q-item>
+                    <q-item-section>
+                        <q-item-label caption>Comment</q-item-label>
+                        <q-item-label>{{clause.comment || 'N/A'}}</q-item-label>
+                    </q-item-section>
+                </q-item>
+                <q-separator inset />
+                <q-item>
+                    <q-item-section>
+                        <q-item-label caption>To</q-item-label>
+                        <q-item-label style="word-break:break-all">
+                            <address-label
+                                :addr="clause.to"
+                                full
+                            >null</address-label>
+                        </q-item-label>
+                    </q-item-section>
+                </q-item>
+                <q-item>
+                    <q-item-section>
+                        <q-item-label caption>Value</q-item-label>
+                        <q-item-label>
+                            <amount-label
+                                :value="clause.value"
+                                :fixed="0"
+                            />
+                        </q-item-label>
+                    </q-item-section>
+                </q-item>
+                <q-item>
+                    <q-item-section>
+                        <q-item-label caption>Data</q-item-label>
+                        <q-item-label>
+                            <q-input
+                                v-if="clause.data && clause.data.length > 2"
+                                dense
+                                class="monospace"
+                                type="textarea"
+                                standout
+                                readonly
+                                :value="clause.data"
+                            />
+                            <template v-else>N/A</template>
+                        </q-item-label>
+                    </q-item-section>
+                </q-item>
+
+            </q-list>
         </q-card>
     </q-dialog>
 </template>
