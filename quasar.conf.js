b91459c1f3d9e8d4373af37f862f46dc253ac4ef
tony <liboliqi@gmail.com>
2021-03-19 16:52:31 +0800

    add notarize scripts

--- quasar.conf.js
+++ quasar.conf.js
@@ -291,7 +291,10 @@ module.exports = configure(function (/* ctx */) {
             target: 'AppImage'
           }
         },
+        afterSign: "build/notarize.js",
         mac: {
+          entitlements: "build/entitlements.mac.plist",
+          entitlementsInherit: "build/entitlements.mac.plist",
           target: {
             arch: ['x64', 'arm64'],
             target: 'dmg'
