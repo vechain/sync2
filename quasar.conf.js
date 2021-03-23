f61a0c20e6354d6fea86fb66d01ec2edb6b8d9a1
qianbin <cola.tin.com@gmail.com>
2021-03-24 00:40:53 +0800

    config: properly config webpack for electron build

--- quasar.conf.js
+++ quasar.conf.js
@@ -151,6 +151,12 @@ module.exports = configure(function (/* ctx */) {
           ...cfg.resolve.alias,
           core: path.resolve(__dirname, './src/core'),
         }
+
+        if(cfg.target === 'electron-renderer') {
+          cfg.externals = cfg.externals || {}
+          cfg.externals['node-hid'] = 'commonjs node-hid'
+        }
+
         if (process.env.NODE_ENV === 'production') {
           // linting is slow in TS projects, we execute it only for production builds
           cfg.module.rules.push({
@@ -313,7 +319,7 @@ module.exports = configure(function (/* ctx */) {
         // do something with Electron main process Webpack cfg
         // chainWebpack also available besides this extendWebpack
 
-        cfg.resolve.extensions.push('.ts')         
+        cfg.resolve.extensions.push('.ts')
         cfg.module.rules.push({
           test: /\.ts$/,
           use :{
