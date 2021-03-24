376df8f49a220e0c476fbad55a696a32af564cba
qianbin <cola.tin.com@gmail.com>
2021-03-24 16:13:21 +0800

    utils: improve ledger connect

--- src/utils/ledger.ts
+++ src/utils/ledger.ts
@@ -1,21 +1,25 @@
 import type Transport from 'ledgerhq__hw-transport'
 
-export const supported = (() => {
-    switch (process.env.MODE) {
-        case 'electron':
-            return true
-        case 'spa':
-        case 'pwa':
-            return 'hid' in window.navigator
-        default: return false
+const connector: { connect(): Promise<Transport> } | null = (() => {
+    if (process.env.MODE === 'electron') {
+        return {
+            connect: () => require('@ledgerhq/hw-transport-node-hid-noevents').default.open('')
+        }
+    } else if (process.env.MODE === 'spa' || process.env.MODE === 'pwa') {
+        if ('hid' in window.navigator) {
+            return {
+                connect: () => require('@ledgerhq/hw-transport-webhid').default.create()
+            }
+        }
     }
+    return null
 })()
 
-export function connect(): Promise<Transport> {
-    if (process.env.MODE === 'electron') {
-        return require('@ledgerhq/hw-transport-node-hid-noevents').default.open('')
-    } else if (process.env.MODE === 'spa' || process.env.MODE === 'pwa') {
-        return require('@ledgerhq/hw-transport-webhid').default.create()
+export const supported = !!connector
+
+export function connect() {
+    if (!connector) {
+        throw new Error('unsupported')
     }
-    return Promise.reject(new Error('unsupported'))
+    return connector.connect()
 }
