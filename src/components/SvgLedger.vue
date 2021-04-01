b2bd487b719da795aadd544298d7e74d971badff
Asbert Ma <stayinalive@msn.cn>
2021-04-01 13:39:52 +0800

    svg for ledger icon

---
+++ src/components/SvgLedger.vue
@@ -0,0 +1,36 @@
+<template>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 23.76 23.76"
+    >
+        <g>
+            <g>
+                <g id="paths">
+                    <path
+                        id="Shape"
+                        d="M16.7,0H7.59V12.29h12.3V3.18A3.18,3.18,0,0,0,16.7,0Z"
+                    />
+                    <path d="M4.75,0H3.18A3.18,3.18,0,0,0,0,3.18V4.75H4.75Z" />
+                    <polygon
+                        id="Rectangle-path"
+                        points="0 7.59 4.75 7.59 4.75 12.34 0 12.34 0 7.59"
+                    />
+                    <path d="M15.18,19.89h1.57a3.18,3.18,0,0,0,3.18-3.19V15.18H15.18Z" />
+                    <polygon points="7.59 15.18 12.34 15.18 12.34 19.93 7.59 19.93 7.59 15.18" />
+                    <path d="M0,15.18v1.57a3.18,3.18,0,0,0,3.18,3.18H4.75V15.18Z" />
+                </g>
+            </g>
+        </g>
+    </svg>
+</template>
+
+<style scoped>
+svg {
+    width: 1em;
+    vertical-align: bottom;
+}
+#paths path,
+#paths polygon {
+    fill: currentcolor;
+}
+</style>
