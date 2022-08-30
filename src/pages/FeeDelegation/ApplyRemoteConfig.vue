eaa9ccc58c8dbad46da21ab8a2ee878b89871bae
Mario Micklisch <mm@hpm.agency>
2022-08-30 20:44:17 +0200

    Improve code structure

--- src/pages/FeeDelegation/ApplyRemoteConfig.vue
+++ src/pages/FeeDelegation/ApplyRemoteConfig.vue
@@ -1,12 +1,11 @@
 <template>
   <q-dialog ref="dialog" @hide="$emit('hide')" maximized transition-show="slide-up" transition-hide="slide-down">
     <q-card class="column no-wrap">
-      <page-toolbar :title="$t('feeDelegation.title')" icon="close" :gid="gid" @action="hide()" />
+      <page-toolbar :title="$t('feeDelegation.title')" icon="close" @action="hide()" />
       <page-content class="col q-pa-sm bg-grey-3">
         {{$t('feeDelegation.accept_remote_config')}}
         <q-card flat class="bg-yellow-1">
-          <q-card-section class="serif" style="min-height:300px; white-space: pre-wrap;">
-            {{JSON.stringify(state, "", 2)}}
+          <q-card-section class="serif" style="min-height:300px; white-space: pre-wrap;">{{  configAsText  }}
           </q-card-section>
         </q-card>
       </page-content>
@@ -21,24 +20,30 @@
 import Vue from 'vue'
 import { QDialog } from 'quasar'
 import PageToolbar from 'components/PageToolbar.vue'
+import PageAction from 'src/components/PageAction.vue'
+import PageContent from 'src/components/PageContent.vue'
 
 export default Vue.extend({
-
-  components: { PageToolbar },
+  components: { PageToolbar, PageAction, PageContent },
   props: {
-    state: Object as () => { }
+    state: Object as () => {}
   },
   computed: {
+    configAsText() {
+      if (this.state === undefined) {
+        return ''
+      }
+
+      return JSON.stringify(this.state, null, 2)
+    }
   },
   methods: {
-    // method is REQUIRED by $q.dialog
+    // method is REQUIRED by $q.dialo
     show() { (this.$refs.dialog as QDialog).show() },
     // method is REQUIRED by $q.dialog
     hide() { (this.$refs.dialog as QDialog).hide() },
-
     ok() {
       this.$emit('ok', true)
-      this.hide()
     }
   }
 })
